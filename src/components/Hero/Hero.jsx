ca64156a7ad9f717b29613780c0d81b23204480c
vinay-colaco <vinaygreavsoncolaco09@gmail.com>
2024-07-27 19:54:32 +0530

    First test deployment

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,11 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Vinay Greavson Colaco</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a software enginer with 6 months of experience using Python. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:vinaygreavsoncolaco09@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
